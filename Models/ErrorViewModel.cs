10f871bee404091c38c2c434f37e6d24e2fd9014
John Doe <john.doe@contoso.com>
2023-08-30 13:23:41 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace munwebbisivu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
